bea92f7b4609ce1bcd6ee756d3cd5718a84f9e23
Andre Kelpe <efeshundertelf@googlemail.com>
2013-06-07 13:51:20 -0700

    copy version.properties file into multitool directory, so that the classloader finds it

---
+++ src/test/java/multitool/util/VersionTest.java
@@ -0,0 +1,11 @@
+package multitool.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fs111
+ * Date: 07.06.13
+ * Time: 12:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class VersionTest {
+}
